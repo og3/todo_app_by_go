e3f2a393487a09c896fa4ff94acbc4197623fa33
noriyuki togashi <>
2020-01-04 10:37:35 +0900

    インデントがずれていたので整形

--- main.go
+++ main.go
@@ -1,170 +1,169 @@
 package main
 
 import (
-    "strconv"
-    // ginのインポート
-    "github.com/gin-gonic/gin"
-    // gormのインポート
-    "github.com/jinzhu/gorm"
-    _"github.com/go-sql-driver/mysql"
+  "strconv"
+  // ginのインポート
+  "github.com/gin-gonic/gin"
+  // gormのインポート
+  "github.com/jinzhu/gorm"
+  _"github.com/go-sql-driver/mysql"
 )
 
 // 機能
 func main() {
-    router := gin.Default()
-    // 各templateの読み込み
-    router.LoadHTMLGlob("templates/*.html")
+  router := gin.Default()
+  // 各templateの読み込み
+  router.LoadHTMLGlob("templates/*.html")
 
-    // DBの初期化
-    dbInit()
+  // DBの初期化
+  dbInit()
 
-    //Index
-    router.GET("/", func(ctx *gin.Context) {
-        // 関数の呼び出し
-        todos := dbGetAll()
-        ctx.HTML(200, "index.html", gin.H{
-            "todos": todos,
-        })
+  //Index
+  router.GET("/", func(ctx *gin.Context) {
+    // 関数の呼び出し
+    todos := dbGetAll()
+    ctx.HTML(200, "index.html", gin.H{
+        "todos": todos,
     })
+  })
 
-    //Create
-    router.POST("/new", func(ctx *gin.Context) {
-        text := ctx.PostForm("text")
-        status := ctx.PostForm("status")
-        dbInsert(text, status)
-        ctx.Redirect(302, "/")
-    })
+  //Create
+  router.POST("/new", func(ctx *gin.Context) {
+    text := ctx.PostForm("text")
+    status := ctx.PostForm("status")
+    dbInsert(text, status)
+    ctx.Redirect(302, "/")
+  })
 
-    //Detail
-    router.GET("/detail/:id", func(ctx *gin.Context) {
-        n := ctx.Param("id")
-        id, err := strconv.Atoi(n)
-        if err != nil {
-            panic(err)
-        }
-        todo := dbGetOne(id)
-        ctx.HTML(200, "detail.html", gin.H{"todo": todo})
-    })
+  //Detail
+  router.GET("/detail/:id", func(ctx *gin.Context) {
+    n := ctx.Param("id")
+    id, err := strconv.Atoi(n)
+    if err != nil {
+        panic(err)
+    }
+    todo := dbGetOne(id)
+    ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+  })
 
-    //Update
-    router.POST("/update/:id", func(ctx *gin.Context) {
-        n := ctx.Param("id")
-        id, err := strconv.Atoi(n)
-        if err != nil {
-            panic("ERROR")
-        }
-        text := ctx.PostForm("text")
-        status := ctx.PostForm("status")
-        dbUpdate(id, text, status)
-        ctx.Redirect(302, "/")
-    })
+  //Update
+  router.POST("/update/:id", func(ctx *gin.Context) {
+    n := ctx.Param("id")
+    id, err := strconv.Atoi(n)
+    if err != nil {
+        panic("ERROR")
+    }
+    text := ctx.PostForm("text")
+    status := ctx.PostForm("status")
+    dbUpdate(id, text, status)
+    ctx.Redirect(302, "/")
+  })
 
-    //削除確認
-    router.GET("/delete_check/:id", func(ctx *gin.Context) {
-        n := ctx.Param("id")
-        id, err := strconv.Atoi(n)
-        if err != nil {
-            panic("ERROR")
-        }
-        todo := dbGetOne(id)
-        ctx.HTML(200, "delete.html", gin.H{"todo": todo})
-    })
+  //削除確認
+  router.GET("/delete_check/:id", func(ctx *gin.Context) {
+    n := ctx.Param("id")
+    id, err := strconv.Atoi(n)
+    if err != nil {
+        panic("ERROR")
+    }
+    todo := dbGetOne(id)
+    ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+  })
 
-    //Delete
-    router.POST("/delete/:id", func(ctx *gin.Context) {
-        n := ctx.Param("id")
-        id, err := strconv.Atoi(n)
-        if err != nil {
-            panic("ERROR")
-        }
-        dbDelete(id)
-        ctx.Redirect(302, "/")
+  //Delete
+  router.POST("/delete/:id", func(ctx *gin.Context) {
+    n := ctx.Param("id")
+    id, err := strconv.Atoi(n)
+    if err != nil {
+        panic("ERROR")
+    }
+    dbDelete(id)
+    ctx.Redirect(302, "/")
+  })
 
-    })
-
-    router.Run()
+  router.Run()
 }
 
 // model設計
 type Todo struct {
-    gorm.Model
-    // カラム　型
-    Text   string
-    Status string
+  gorm.Model
+  // カラム　型
+  Text   string
+  Status string
 }
 
 //DBマイグレート
 func dbInit() {
-    // gorm.Open("使用するDBデバイス名", "ファイル名")
-    db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
-    if err != nil {
-        panic("データベース開けず！（dbInit）")
-    }
-    // マイグレートの実行
-    db.AutoMigrate(&Todo{})
-    // DBを閉じる
-    defer db.Close()
+  // gorm.Open("使用するDBデバイス名", "ファイル名")
+  db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
+  if err != nil {
+      panic("データベース開けず！（dbInit）")
+  }
+  // マイグレートの実行
+  db.AutoMigrate(&Todo{})
+  // DBを閉じる
+  defer db.Close()
 }
 
 //DB追加
 func dbInsert(text string, status string) {
-    db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
-    if err != nil {
-        panic("データベース開けず！（dbInsert)")
-    }
-    // DBのcreate
-    db.Create(&Todo{Text: text, Status: status})
-    defer db.Close()
+  db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
+  if err != nil {
+      panic("データベース開けず！（dbInsert)")
+  }
+  // DBのcreate
+  db.Create(&Todo{Text: text, Status: status})
+  defer db.Close()
 }
 
 //DB全取得
 func dbGetAll() []Todo {
-    db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
-    if err != nil {
-        panic("データベース開けず！(dbGetAll())")
-    }
-    var todos []Todo
-    db.Order("created_at desc").Find(&todos)
-    db.Close()
-    return todos
+  db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
+  if err != nil {
+      panic("データベース開けず！(dbGetAll())")
+  }
+  var todos []Todo
+  db.Order("created_at desc").Find(&todos)
+  db.Close()
+  return todos
 }
 
 //DB一つ取得
 func dbGetOne(id int) Todo {
-    db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
-    if err != nil {
-        panic("データベース開けず！(dbGetOne())")
-    }
-    var todo Todo
-    db.First(&todo, id)
-    db.Close()
-    return todo
+  db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
+  if err != nil {
+      panic("データベース開けず！(dbGetOne())")
+  }
+  var todo Todo
+  db.First(&todo, id)
+  db.Close()
+  return todo
 }
 
 //DB更新
 func dbUpdate(id int, text string, status string) {
-    db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
-    if err != nil {
-        panic("データベース開けず！（dbUpdate)")
-    }
-    var todo Todo
-    db.First(&todo, id)
-    todo.Text = text
-    todo.Status = status
-    db.Save(&todo)
-    db.Close()
+  db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
+  if err != nil {
+      panic("データベース開けず！（dbUpdate)")
+  }
+  var todo Todo
+  db.First(&todo, id)
+  todo.Text = text
+  todo.Status = status
+  db.Save(&todo)
+  db.Close()
 }
 
 //DB削除
 func dbDelete(id int) {
-    db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
-    if err != nil {
-        panic("データベース開けず！（dbDelete)")
-    }
-    var todo Todo
-    // 削除対象の読み込み
-    db.First(&todo, id)
-    // 削除
-    db.Delete(&todo)
-    db.Close()
+  db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo_app_by_go?parseTime=true")
+  if err != nil {
+      panic("データベース開けず！（dbDelete)")
+  }
+  var todo Todo
+  // 削除対象の読み込み
+  db.First(&todo, id)
+  // 削除
+  db.Delete(&todo)
+  db.Close()
 }
